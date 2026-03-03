536292b5b78d666963ad6349591b5aec7a233fa3
PLG <PLGgroup27@gmail.com>
2026-03-03 10:42:03 +0700

    update2

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,36 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// #define CKC_DEBUG
+#define BUTTON_MODE
+const char *SSID = "MakerSpaceLab_2.4Ghz";
+const char *PASS = "Maker2025";
+#include <CKC.h>
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(26, OUTPUT);
+  CKC.init(SSID, PASS);
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  CKC.run();
+  if (CKC.CkC_Connected())
+  {
+    digitalWrite(26, 1);
+  }
+  else
+  {
+    digitalWrite(26, 0);
+  }
+  mqttClient.subscribe("PLG");
+  static unsigned long sendTimer = 0;
+  if (millis() - sendTimer > 3000)
+  {
+    sendTimer = millis();
+    
+    MQTT.sendData("NHIET_DO", "PLG");
+    MQTT.sendData("DO_AM", "UPDATE");
+  }
 }
